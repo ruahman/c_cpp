7a767844eb62fa60f4f72e4e3e60bf3976a79856
diego vila <ruahman@gmail.com>
2022-11-03 20:50:18 -0400

    main

--- c_tutorial/main.c
+++ c_tutorial/main.c
@@ -1,23 +1,23 @@
-#include "includes/variables.h"
 #include "includes/arrays.h"
-#include "includes/functions.h"
 #include "includes/conditions.h"
-#include "includes/struct.h"
+#include "includes/files.h"
+#include "includes/functions.h"
+#include "includes/io.h"
 #include "includes/loops.h"
 #include "includes/pointers.h"
-#include "includes/files.h"
-#include "includes/io.h"
+#include "includes/struct.h"
+#include "includes/variables.h"
 
 int main() {
-    variables_demo();
-    arrays_demo();
-    functions_demo();
-    conditions_demo();
-    struct_demo();
-    loops_demo();
-    pointers_demo();
-    files_demo();
-    io_demo();
+  variables_demo();
+  arrays_demo();
+  functions_demo();
+  conditions_demo();
+  struct_demo();
+  loops_demo();
+  pointers_demo();
+  files_demo();
+  io_demo();
 
-    return 0;
+  return 0;
 }
