a53300325b5bdc5e639fa682e4160adc743ac058
ruahman <ruahman@gmail.com>
2025-11-30 03:09:32 +0000

    cmake tut

---
+++ c_cmake_tut/types.c
@@ -0,0 +1,15 @@
+#include <stdbool.h>  //for bool
+#include <stdio.h>
+
+int main() {
+    int a; // Integer
+    unsigned int b; // Unsigned integers only store positive numbers. As a result, they have a higher positive range.
+    char c; // Character
+    short d; // Short integer
+    long e; // Long integer
+    float f; // Floating point integer
+    double g; // Double-precision floating point integer
+    bool h; // Boolean TRUE or FALSE
+    printf("this is types");
+    return 0;
+}
---
+++ c_cmake_tut/types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    int a; // Integer
+    unsigned int b; // Unsigned integers only store positive numbers. As a result, they have a higher positive range.
+    char c; // Character
+    short d; // Short integer
+    long e; // Long integer
+    float f; // Floating point integer
+    double g; // Double-precision floating point integer
+    bool h; // Boolean TRUE or FALSE
+    auto k = 1; // Automatically infer type. Not a type in itself.
+  //
+    std::cout << "types c++" << std::endl;
+    return 0;
+}
